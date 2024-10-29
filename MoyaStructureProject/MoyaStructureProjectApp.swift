ae99b80ec21155fc62023470e6a0f64fd48f54cb
asukim <asukim2020@gmail.com>
2024-10-29 14:22:22 +0900

    first commit

---
+++ MoyaStructureProject/MoyaStructureProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoyaStructureProjectApp.swift
+//  MoyaStructureProject
+//
+//  Created by 김현구 on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MoyaStructureProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
